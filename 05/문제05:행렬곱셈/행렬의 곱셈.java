73d19b6e195fcf3c6dd186d79df87fbe4e3a8c74
김민석 <skyfox001015@naver.com>
2025-01-07 18:57:50 +0900

    feat : 문제 5번- 행렬의 곱셈

---
+++ 05/문제05:행렬곱셈/행렬의 곱셈.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int arr1Row = arr1.length;
+        int arr1Col = arr1[0].length;
+        int arr2Row = arr2.length;
+        int arr2Col = arr2[0].length;
+
+        int[][] answer = new int[arr1Row][arr2Col];
+
+        for(int i = 0 ; i < arr1Row;i++){ //행 순서대로
+            for(int j = 0 ; j< arr2Col ; j++){
+                for(int k = 0; k < arr1Col; k++){
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
+/*
+1. 두 행렬의 행과 열을 저장한다.
+2. 행 순서대로 계산을 한다(1행 1열, 1행 2열..., 2행 1열, 2행 1열...)
+3. 
+*/
